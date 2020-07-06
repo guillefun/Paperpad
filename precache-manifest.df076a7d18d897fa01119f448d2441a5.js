327dd5c4895a61dfec739019d6c83d2a128cf585
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-07-06 13:58:27 +0000

    Deploy guillefun/Paperpad to github.com/guillefun/Paperpad.git:gh-pages

---
+++ precache-manifest.df076a7d18d897fa01119f448d2441a5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e66cce43dbde91e1275eb9852925ebb",
+    "url": "/Paperpad/index.html"
+  },
+  {
+    "revision": "a5a2c0c2a8bb1c9607fe",
+    "url": "/Paperpad/static/css/main.90f79033.chunk.css"
+  },
+  {
+    "revision": "b1dfedf63b97def4e3b0",
+    "url": "/Paperpad/static/js/2.ac7c20c0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Paperpad/static/js/2.ac7c20c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5a2c0c2a8bb1c9607fe",
+    "url": "/Paperpad/static/js/main.da7e851d.chunk.js"
+  },
+  {
+    "revision": "b00699f28a57a69b933c",
+    "url": "/Paperpad/static/js/runtime-main.dab1eef8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Paperpad/static/media/logo.5d5d9eef.svg"
+  }
+]);
